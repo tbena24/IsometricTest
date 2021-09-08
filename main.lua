7586a3feb46e724b94c03ad0d8f192bb26824055
tbena24 <89610659+tbena24@users.noreply.github.com>
2021-09-08 09:40:27 -0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,15 @@
+--# Constants
+local grid_size = 20
+
+--# Variables
+local Block = love.graphics.newImage("block_test.png")
+
+
+--# Functions
+local Block = {}
+
+function Block.new(Image)
+  local newBlock = {
+    newBlock.width
+  }
+end
