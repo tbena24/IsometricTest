83ff42871662567a8e9020aa5937a39bb84ee55f
tbena24 <89610659+tbena24@users.noreply.github.com>
2021-09-08 10:03:31 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,43 +2,53 @@
 local grid_size = 20
 
 --# Variables
-local blockImage = love.graphics.newImage("block_test.png")
+--local blockImage = love.graphics.newImage("block_test.png")
 
 local Blocks = {}
 local Grid = {}
 
 local Block = {}
 
---# Block Module
+--[[# Block Module
 function Block.new(Image)
   local newBlock = {
     width = Image:getWidth(),
     height = Image:getHeight(),
     depth = height / 2
   }
-end
+end]]
 
 --# Execution
 for x = 1, grid_size do
-  grid[x] = {}
+  Grid[x] = {}
   for y = 1, grid_size do
-    grid[x][y] = 1
+    Grid[x][y] = 1
   end
 end
 
-grid[2][4] = 2
-grid[6][5] = 2
+local block_width = 30
+local block_height = 30
+local block_depth = 30/2
+local grid_x = 0
+local grid_y = 0
 
-for x = 1,grid_size do
-   for y = 1,grid_size do
-      if grid[x][y] == 1 then
-         love.graphics.draw(blockImage,
-            grid_x + ((y-x) * (block_width / 2)),
-            grid_y + ((x+y) * (block_depth / 2)) - (block_depth * (grid_size / 2)))
-      else -- grid[x][y] == 2
-         love.graphics.draw(blockImage,
-            grid_x + ((y-x) * (block_width / 2)),
-            grid_y + ((x+y) * (block_depth / 2)) - (block_depth * (grid_size / 2)))
-      end
-   end
+Grid[2][4] = 2
+Grid[6][5] = 2
+
+function love.draw()
+  for x = 1,grid_size do
+     for y = 1,grid_size do
+        if Grid[x][y] == 1 then
+           love.graphics.rectangle("line",
+              grid_x + ((y-x) * (block_width / 2)),
+              grid_y + ((x+y) * (block_depth / 2)) - (block_depth * (grid_size / 2)),
+              block_width, block_height)
+        else -- Grid[x][y] == 2
+           love.graphics.rectangle("line",
+              grid_x + ((y-x) * (block_width / 2)),
+              grid_y + ((x+y) * (block_depth / 2)) - (block_depth * (grid_size / 2)),
+              block_width, block_height)
+        end
+     end
+  end
 end
